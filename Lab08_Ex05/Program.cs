de051e1691377c30feba4bb9360fd92fbd736626
jirasinworasiri <aomjirasin457@gmail.com>
2025-03-25 22:42:14 +0700

    66030029 จิรสิน

---
+++ Lab08_Ex05/Program.cs
@@ -0,0 +1,19 @@
+﻿var myObj = new MyClass();
+myObj.Info();
+
+class MyClass
+{
+    int F1;
+    string F2;
+    int F3 = 100;
+    string F4 = "ASDF";
+
+     void Info()
+     }
+        System.Console.WriteLine($"Default integer      F1 = {F1}");  //Implicit fields initialization
+        Private void Info()
+
+        System.Console.WriteLine($"Initialized integer  F3 = {F3}");  //Explicit field initialization
+        System.Console.WriteLine($"Initialized string   F4 = {F4}");
+    }
+}
